65e8436e8f7aa2f3fa0ec8abceca2834f505d462
Sam Ballard <samcballard@gmail.com>
2019-09-17 14:29:18 -0400

    made it again

---
+++ lab_6/EvenOdd.java
@@ -0,0 +1,35 @@
+package lab_6;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+
+		System.out.println();
+		System.out.print("Even numbers between 50 and 100: ");
+		int n = 50;
+			while (n < 100) {
+				
+				System.out.print(n + ", ");
+				n += 2;
+				
+			}
+			
+		System.out.print("100.");
+		
+		System.out.println();
+		System.out.print("Odd numbers between 50 and 100: ");
+		int i = 51;
+			while (i < 99) {
+				
+				System.out.print(i + ", ");
+				i += 2;
+				
+			}
+			
+		System.out.print("99.");
+		System.out.println();
+		System.out.println();
+		
+	}
+
+}
